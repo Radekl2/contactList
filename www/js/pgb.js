cc1329cfe5caad9e71ac919d5440f17ce909d476
Radoslaw Smyksy <rsmyksy@gmail.com>
2018-03-12 12:48:29 +0100

    code changes

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -9,19 +9,17 @@ function onDeviceReady() {
 function showContactList() {
 	var table = document.getElementById("tab");
 	navigator.contactsPhoneNumbers.list(function(contacts) {
-      console.log(contacts.length + ' contacts found');
+      //console.log(contacts.length + ' contacts found');
       for(var i = 0; i < contacts.length; i++) {
-		  console.log(contacts[i].id + " - " + contacts[i].displayName);
+		  //console.log(contacts[i].id + " - " + contacts[i].displayName);
 		  var row = table.insertRow(0);
 		  var cell1 = row.insertCell(0);
-		  cell1.innerHTML = "Name: " + contacts[i].displayName;
-		  document.getElementById('cont').innerHTML = 'Name: ' + contacts[i].displayName;
+		  cell1.innerHTML = " " + contacts[i].displayName;
          for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
             var phone = contacts[i].phoneNumbers[j];
-			console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
-			document.getElementById('cont2').innerHTML = 'Number: ' + phone.number;
+			//console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
 			var cell2 = row.insertCell(0);
-			cell2.innerHTML = "Number: " + phone.number;
+			cell2.innerHTML = "Number and name: " + phone.number;
          }
       }
    }, function(error) {
