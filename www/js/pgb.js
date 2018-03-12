5bbac83d8d3fc662e4dd49c5f8ab4606af445a42
Radoslaw Smyksy <rsmyksy@gmail.com>
2018-03-12 12:14:43 +0100

    files changed

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -3,12 +3,23 @@ function init() {
 }
 
 function onDeviceReady() {
-	navigator.notification.beep(1);
+	showContactList()
 }
 
-function showNetworkInfo() {
-	info = WifiWizard.isWifiEnabled();
-
-navigator.notification.alert(info);
+function showContactList() {
+	
+	navigator.contactsPhoneNumbers.list(function(contacts) {
+      console.log(contacts.length + ' contacts found');
+      for(var i = 0; i < contacts.length; i++) {
+         console.log(contacts[i].id + " - " + contacts[i].displayName);
+         for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+            var phone = contacts[i].phoneNumbers[j];
+			document.getElementById('cont').innerHTML = 'Your contatcts: ' + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")";
+         }
+      }
+   }, function(error) {
+      console.error(error);
+   });
+	
 }
 
