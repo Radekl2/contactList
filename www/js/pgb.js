a27e8a5de7569e98b214f5d8636a5e38f70837a2
Radoslaw Smyksy <rsmyksy@gmail.com>
2018-03-12 12:36:42 +0100

    bug fixes

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -11,11 +11,12 @@ function showContactList() {
 	navigator.contactsPhoneNumbers.list(function(contacts) {
       console.log(contacts.length + ' contacts found');
       for(var i = 0; i < contacts.length; i++) {
-		  //var contact = 'Name' + contacts[i].displayName);
+		  console.log(contacts[i].id + " - " + contacts[i].displayName);
+		  document.getElementById('cont').innerHTML = 'Name: ' + contacts[i].displayName;
          for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
             var phone = contacts[i].phoneNumbers[j];
-			contact = ' number: ' +phone.number:
-			document.getElementById('cont').innerHTML = 'Contact list: ' + contact;
+			console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
+			document.getElementById('cont2').innerHTML = 'Number: ' + phone.number;
          }
       }
    }, function(error) {
